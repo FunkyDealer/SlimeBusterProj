d5ea9559202c9c930b2d33abdee2fd160ad8e4d7
Pedro Almeida <petergames@live.com.pt>
2022-07-21 17:10:34 +0100

    Added Sound Controls for the options menu

--- SlimeBustersProj/Assets/Scripts/Menu.cs
+++ SlimeBustersProj/Assets/Scripts/Menu.cs
@@ -1,22 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField]
+    private string FirstLevelName = "Ranch";
 
-    public void playgame()
+    [SerializeField]
+    private Slider musicSlider;
+    [SerializeField]
+    private TMP_Text musicVolumeNumberText;
+    [SerializeField]
+    private Slider effectsSlider;
+    [SerializeField]
+    private TMP_Text effectsVolumeNumberText;
+
+    private void Start()
     {
-        SceneManager.LoadScene("Ranch");
+        musicSlider.onValueChanged.AddListener(delegate { ChangeMusicVolume(); });
+        effectsSlider.onValueChanged.AddListener(delegate { ChangeEffectsVolume(); });
+
+        musicVolumeNumberText.text = musicSlider.value.ToString();
+        effectsVolumeNumberText.text = effectsSlider.value.ToString();
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(FirstLevelName);
     }
          
     
-      public void quitgame()
-        {
-            Debug.Log("Quit");
-            Application.Quit();
-        }
+    public void QuitGame()
+    {
+        //Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void ChangeMusicVolume()
+    {
+        int value = (int)musicSlider.value;
+        musicVolumeNumberText.text = value.ToString();
+        AkSoundEngine.SetRTPCValue("MusicVolume", value);
+        
+    }
+
+    public void ChangeEffectsVolume()
+    {
+        int value = (int)effectsSlider.value;
+        effectsVolumeNumberText.text = value.ToString();
+        AkSoundEngine.SetRTPCValue("EffectsVolume", value);
+
+    }
+
+
+
 
     
 }
--- SlimeBustersProj/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ SlimeBustersProj/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -36,6 +36,12 @@ namespace AK
         static const AkUniqueID STOP_VACUUM = 3044255001U;
     } // namespace EVENTS
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID EFFECTSVOLUME = 640585335U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
